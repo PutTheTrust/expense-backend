df46617ae60f5e6a781ecb75a19e6d681179f2d0
Bekithemba <putthetrust.gamedev@gmail.com>
2023-07-20 07:44:17 +0200

    add: login endpoint

--- controllers/userController.js
+++ controllers/userController.js
@@ -32,3 +32,38 @@ exports.signup = async (req, res) => {
     console.log(e);
   }
 };
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // 1) Check if email and password exist
+    if (!email || !password) {
+      return res.status(400).json({
+        status: "fail",
+        messege: "Please provide email and password",
+      });
+    }
+
+    // 2) Check if user exists && password is correct
+    const user = await User.findOne({ email }).select("+password");
+
+    if (!user || !(await user.correctPassword(password, user.password))) {
+      //   return next(new AppError('Incorrect email or password', 401));
+      return res.status(401).json({
+        status: "fail",
+        messege: "Incorrect email or password",
+      });
+    }
+
+    // 3) If everything is ok, send to client
+
+    const token = signToken(user._id);
+    res.status(200).json({
+      status: "success",
+      token,
+    });
+  } catch (e) {
+    console.log(e);
+  }
+};
--- models/userModel.js
+++ models/userModel.js
@@ -1,8 +1,17 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true, select: false },
 });
+
+// Instance method is available in all documents of a certain collection
+userSchema.methods.correctPassword = async function (
+  candidatePassword,
+  userPassword
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
 
 module.exports = mongoose.model("User", userSchema);
