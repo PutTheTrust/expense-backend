6cb8811baf835a8d78d8fc9524b508e2c2a75a3c
Bekithemba <putthetrust.gamedev@gmail.com>
2023-08-09 18:30:44 +0200

    update: cors

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://expense-backend-beta.vercel.app/",
+      "https://expense-backend-beta.vercel.app",
     ],
   })
 );
